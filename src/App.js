84eb63201a19401902be5b5751a344744783d282
Denny29 <43240668+Denny29@users.noreply.github.com>
2021-10-25 18:26:32 -0400

    class list Basic review.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./styles.css";
 
 export default function App() {
+  function List(parm1, parm2, parm3) {
+    return (
+      <ul>
+        <li>{parm1}</li>
+        <li>{parm2}</li>
+        <li>{parm3}</li>
+      </ul>
+    );
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {List("Denny F.", "Student", "blue")}
+      {List("Jake", "Student", "red")}
     </div>
+  );
+}
+
+function List(parm1, parm2, parm3) {
+  return (
+    <ul>
+      <li>{parm1}</li>
+      <li>{parm2}</li>
+      <li>{parm3}</li>
+    </ul>
   );
 }
